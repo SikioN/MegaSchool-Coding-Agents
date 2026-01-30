0d052ec8ab83ea1bd9fc5b336efb4091332e6916
NMuravya <nmuravya@mail.ru>
2026-01-30 11:09:35 +0300

    Решение задачи 11

---
+++ src/math_utils.py
@@ -0,0 +1,12 @@
+def add(a, b):
+    """
+    Возвращает сумму двух чисел.
+
+    Args:
+        a (int or float): первое число.
+        b (int or float): второе число.
+
+    Returns:
+        int or float: сумма a и b.
+    """
+    return a + b
