3b742a4ddfbd72c8fefda9bf9efc86700e2f761c
runner <runner@runnervmkj6or>
2026-01-30 15:04:47 +0000

    Решение задачи 39

---
+++ src/utils/logger.py
@@ -0,0 +1,23 @@
+import logging
+
+def setup_logger(name: str) -> logging.Logger:
+    """
+    Configures and returns a logger instance.
+    
+    Args:
+        name (str): The name of the logger.
+        
+    Returns:
+        logging.Logger: The configured logger instance.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(formatter)
+    
+    logger.addHandler(console_handler)
+    
+    return logger
